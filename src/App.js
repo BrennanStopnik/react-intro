550c84e816d5a61bbf3d7bcd6cafc6b7e1d5bac4
BrennanStopnik <brennan.stopnik@codeimmersives.com>
2022-10-04 12:29:57 -0400

    Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const HeaderInfo = () => {
+	return (
+		<div>
+			<h1>My Counter</h1>
+			<p>Wanted to make this without looking at the source code I learned it from.</p>
+			<hr/>
+			<br/>
+		</div>
+	)
+}
+
+const FullName = (names) => {
+	return (
+		<div>
+			<br/>
+			<hr/>
+			<p>Made By:</p>
+			<h3 className='my-name'>{names.firstName + " " + names.lastName}</h3>
+		</div>
+	)
+}
+
+const App = () => {
+	const firstName = "Brennan"
+	const lastName = "Stopnik"
+
+	const [count, setCount] = useState(0)
+	// const [color, setColor] = useEffect()
+
+	const decrementCount = () => {
+		setCount(prevCount => prevCount - 1)
+	}
+
+	const incrementCount = () => {
+		setCount(prevCount => prevCount + 1)
+	}
+
+	// const changeColor = () => {
+	// 	setColor()
+	// }
+
+  	return (
+    	<div className="App">
+			<HeaderInfo />
+			<button className='button' onClick={decrementCount}>-</button>
+			<span className='count'>{count}</span>
+			<button className='button' onClick={incrementCount}>+</button>
+			<FullName 
+				firstName={firstName}
+				lastName={lastName}
+			/>
+    	</div>
+  	);
 }
 
 export default App;
